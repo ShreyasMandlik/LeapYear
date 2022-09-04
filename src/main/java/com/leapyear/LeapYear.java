cbb125096b5efdac917b81a9066a3db7a83d7b06
Shreyas <mandlikshreyas01@gmail.com>
2022-09-04 16:42:27 +0530

    Intial commit

---
+++ src/main/java/com/leapyear/LeapYear.java
@@ -0,0 +1,7 @@
+package com.leapyear;
+import  java.util.Scanner;
+public class LeapYear {
+    public static void main(String args[]){
+
+    }
+}
---
+++ src/test/java/com/leapyear/LeapYearTest.java
@@ -0,0 +1,12 @@
+package com.leapyear;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeapYearTest {
+
+
+
+}
