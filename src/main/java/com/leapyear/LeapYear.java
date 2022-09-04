06e7c3ef1536b65a028adb13afb7889e4c9a7ff8
Shreyas <mandlikshreyas01@gmail.com>
2022-09-04 16:52:40 +0530

    Main Program written

--- src/main/java/com/leapyear/LeapYear.java
+++ src/main/java/com/leapyear/LeapYear.java
@@ -2,6 +2,33 @@ package com.leapyear;
 import  java.util.Scanner;
 public class LeapYear {
     public static void main(String args[]){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number of Years you want to check : ");
+        int No_Year = sc.nextInt();
+        while (No_Year!=0) {
+            System.out.println("Enter the Year you want to check : ");
+            int Year=sc.nextInt();
+            if(checkYear(Year)==Boolean.TRUE){
+                System.out.println("The is a leap Year ");
+            }
+            else{
+                System.out.println("The is a not leap Year ");
+            }
+            No_Year--;
+        }
     }
+
+    public static boolean checkYear(int Year){
+        if (Year % 4 == 0) {
+            if (Year % 100 == 0) {
+                if (Year % 400 == 0)
+                    return Boolean.TRUE;
+                else
+                    return Boolean.FALSE;
+            } else
+                return Boolean.TRUE;
+        } else
+            return Boolean.FALSE;
+    }
+
 }
